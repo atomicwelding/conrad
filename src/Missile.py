9772903eda30881df7603084d22ef353ff749df4
atomicwelding <ctfweld@gmail.com>
2024-09-29 16:53:25 +0200

    Delete src/Missile.py : has been replaced by src/Projectile.py

--- src/Missile.py
+++
@@ -1,8 +0,0 @@
-from IEntity import *
-
-class Missile(IEntity):
-    def __init__(self, mass = 1., rr = 0., rtheta = 0., rdot = 0., thetadot = 0.):
-        super().__init__(mass,rr,rtheta,rdot,thetadot)
-
-    def draw(self):
-        raise NotImplementedError
