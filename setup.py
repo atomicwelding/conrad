9cbbb9962c28745495fd6caa42e9f357c219b1c0
atomicwelding <ctfweld@gmail.com>
2024-09-29 16:50:25 +0200

    changed name of the game

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ from setuptools import setup
 
 
 setup(
-    name='space-battle-black-hole',
+    name='conrad',
     version='0.0.1',
-    description='A 2D space battle around a black hole video game',
+    description='A 2D space battle game around a black hole',
     url='https://github.com/atomicwelding/space-battle-black-hole',
     author='Erwan Le Doeuff (weld)',
     author_email='erwan.le_doeuff@edu.univ-fcomte.fr',
-    install_requires=[],  # Add any dependencies here if needed
+    install_requires=[
+        'pygame'
+    ],
 )
