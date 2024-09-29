cb68a7e07fe37ff37ebae6254fb4739d1eb51b64
atomicwelding <ctfweld@gmail.com>
2024-09-29 16:09:41 +0200

    waiting for

--- src/App.py
+++ src/App.py
@@ -1,10 +1,16 @@
 from CustomConsole import CustomConsole
 from StateManager import StateManager
 
+# entities
+from BlackHole import BlackHole
+from Player import Player
+from EntityPool import EntityPool 
+
 import pygame
 import threading
 import time
 import sys
+import os 
 
 
 class App():
@@ -37,12 +43,34 @@ class App():
     def game_loop(self):
         # launch pygame
         pygame.init()
-        screen = pygame.display.set_mode((640, 480))
-        pygame.display.set_caption("Hello World")
+        screen = pygame.display.set_mode((512, 512))
+        pygame.display.set_caption("Conrad")
 
+
+
+        entity_pool = EntityPool()
+        
+        # instantiating entities
+        bh = BlackHole()
+        entity_pool.add(bh)
+        
+        player = Player(rr = self.sm.get('distance'),
+                        rdot = self.sm.get('radial_velocity'),
+                        thetadot = self.sm.get('angular_velocity') / self.sm.get('distance') )
+        entity_pool.add(player)
+
+
+        
+
+
+        background = pygame.image.load(os.path.join('assets', 'green_bg.png')).convert()
+        screen.blit(background, (0,0))
+        pygame.display.flip()
+        
         # game loop
         while True:
             self.check_shouldExit()
+            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
