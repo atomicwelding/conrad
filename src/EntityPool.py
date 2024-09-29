6a47abca9d66a7e29e0dc9a34133e9361e346504
atomicwelding <ctfweld@gmail.com>
2024-09-29 15:58:21 +0200

    Entity pool

---
+++ src/EntityPool.py
@@ -0,0 +1,18 @@
+from IEntity import IEntity
+
+class EntityPool():
+
+
+    def __init__():
+        self.pool = {}
+
+
+    def add(self, entity : IEntity):
+        self.pool[entity.id] = entity
+
+    def remove(self, entity : IEntity):
+        self.pool.pop(entity.id)
+
+    def  drawPool(self, screen):
+        for id in self.pool:
+            self.pool[id].draw()
