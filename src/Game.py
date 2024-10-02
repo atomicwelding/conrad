361d896c33d44c2a38f8deb34b3c2d2164c24134
atomicwelding <ctfweld@gmail.com>
2024-10-02 13:42:47 +0200

    huge refactor of gameloop

--- src/Game.py
+++ src/Game.py
@@ -26,10 +26,6 @@ SCREEN_HEIGHT = 512
 ASSETS_PATH = 'assets'
 BACKGROUND_PATH = 'green_bg.png'
 
-##### TODO
-
-#### RESOUDRE LE PROBLEME DE FACTEUR 
-
 # physical constants
 G = 6.67*10e-11
 c = 1
@@ -43,24 +39,157 @@ M = 1/(2*G)
 m = alpha * M
 mu = M*G
 
+radial_acc = lambda entity: ( - G*M/entity.rr**2 ) + (entity.rr - 3/2 * R_S) * (entity.l0**2)/(entity.rr**4)
+
 # physical integration 
 dt = 0.01
-T = 1
+T = 2
 nb_steps = int(T / dt)
+
+
+
 
 class Game():
     def __init__(self):
+        
         self.sm = StateManager(state = {
             'canRun':False,
             'shouldExit':False,
             'playerTurn':False,
 
+            'playerShot': False,
             
             'distance':None,
             'angular_velocity':None,
             'radial_velocity':None,
         })
 
+
+        # init pygame & scene
+        pygame.init()
+        pygame.font.init() 
+        self.scene = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE)
+
+        # init some helper msgs 
+        self.text_your_turn = self.init_text('Your turn!')
+        self.text_computing = self.init_text('Computing new positions ...')
+        self.text_you_won = self.init_text('You won')
+        self.text_you_lose = self.init_text('You lose')
+
+        # init text that will be displayed
+        self.current_text = self.text_your_turn
+
+        # create the entity pool and add some entities
+        self.entity_pool = EntityPool()
+
+        # set the background image
+        self.background = pygame.image.load(os.path.join(ASSETS_PATH, BACKGROUND_PATH)).convert()
+
+        # create black hole, player and target and add them to pool
+        self.black_hole = BlackHole(mass = M, R_S = R_S)
+        self.entity_pool.add(self.black_hole)
+        
+        self.player = Player(mass = m,
+                             rr = 0,
+                             rdot = 0,
+                             thetadot = 0)
+        self.entity_pool.add(self.player)
+
+        self.target = Target(mass = m,
+                             rr = 0,
+                             rdot = 0,
+                             thetadot = 0)
+        self.entity_pool.add(self.target)
+
+
+
+        
+
+    def update_entities(self):
+        # iterate over entities
+        for entity in self.entity_pool.pool:
+            current = self.entity_pool.pool[entity]
+            if(not current.palive):
+                continue
+        
+            current.update(self.scene, radial_acc, dt)
+                
+            # test for collisions
+            for other_entity in self.entity_pool.pool:
+                other_current = self.entity_pool.pool[other_entity]
+                if(not other_current.palive or current.id == other_entity):
+                    continue    
+                current.is_colliding_with(other_current)
+                        
+
+
+
+
+                
+    def draw_scene(self):
+        self.scene.blit(self.background, (0,0))
+        self.scene.blit(self.current_text, (0,0))
+        for entity in self.entity_pool.pool:
+            if(self.entity_pool.pool[entity].palive): 
+                self.entity_pool.pool[entity].draw(self.scene)
+        pygame.display.flip()
+
+
+
+        
+        
+    def gameloop(self):
+        distance = self.sm.get('distance')
+        angular_velocity = self.sm.get('angular_velocity')
+        radial_velocity = self.sm.get('radial_velocity')
+
+        self.player.rr = distance
+        self.player.rtheta = 0
+        self.player.rdot = radial_velocity
+        self.player.thetadot = angular_velocity / distance
+
+
+        self.target.rr = distance
+        self.target.rtheta = np.pi
+        self.target.rdot = radial_velocity
+        self.target.thetadot = angular_velocity / distance
+
+        self.draw_scene()
+
+        while True:
+            if(not self.sm.get('playerTurn')): # computer's turn
+                self.current_text = self.text_computing
+                for _ in range(nb_steps):
+                    self.update_entities()
+                    self.draw_scene()
+                self.sm.set('playerTurn', True)
+
+            else: # player's turn
+                self.current_text = self.text_your_turn
+
+
+            # exiting conditions
+            if(not self.player.palive):
+                self.current_text = self.text_you_lose
+                self.sm.set('shouldExit', True)
+            elif(not self.target.palive):
+                self.current_text = self.text_you_won
+                self.sm.set('shouldExit', True)
+
+
+            self.draw_scene()
+            self.should_game_exit()
+
+
+            # event loop of pygame
+            for event in pygame.event.get():
+                if(event.type == pygame.QUIT):
+                    pygame.quit()
+                    sys.exit()
+
+
+        
+        
     def console_handler(self):
         console = CustomConsole(state_manager=self.sm)
         welcome_msg = """
@@ -86,114 +215,7 @@ Type `list [commands|variables]` to list all the commands/variables.`
         font = pygame.font.Font(os.path.join(ASSETS_PATH, 'golden-age.ttf'), size)
         return font.render(txt, False, (217,0,210))
 
-    def has_won(self, scene, predicate):
-        msg = "You won"
-        if(not predicate):
-            msg = "You lose"
-
-        text_exiting = self.init_text(msg, size = 100)
-        w,h = text_exiting.get_size()
-        scene.blit(text_exiting, (SCREEN_WIDTH//2 - w//2, SCREEN_HEIGHT//2 - h // 2))
-        pygame.display.flip()
-        self.sm.set('shouldExit', True)  
-
-    def gameloop(self):
-        # launch pygame
-        pygame.init()
-        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE)
-        pygame.display.set_caption(GAME_TITLE)
-
-        # create some text
-        pygame.font.init()
-        
-        text_your_turn = self.init_text('Your turn!')
-        text_computing = self.init_text('Computing new positions ...')
-
-        # instantiating entities in the pool
-        entity_pool = EntityPool()
-
-        bh = BlackHole(mass = M, R_S = R_S)
-        entity_pool.add(bh)
-
-        distance = self.sm.get('distance')
-        radial_velocity = self.sm.get('radial_velocity')
-        angular_velocity = self.sm.get('angular_velocity')
-
-        player = Player(mass = m,
-                        rr = distance,
-                        rdot = radial_velocity ,
-                        thetadot = angular_velocity / distance)
-        #entity_pool.add(player)
-
-      
-        target = Target(mass = m,
-                        rr = distance,
-                        rdot = radial_velocity ,
-                        thetadot = angular_velocity / distance)
-        entity_pool.add(target)
-
-
-        # setup the background image
-        background = pygame.image.load(os.path.join(ASSETS_PATH,BACKGROUND_PATH)).convert()
-
-        # init the scene
-        screen.blit(background, (0,0))
-        screen.blit(text_computing, (0,0))
-        for entity in entity_pool.pool:
-                        entity_pool.pool[entity].draw(screen)
-        pygame.display.flip()
-
-
-
-        # en attendant
-        radial_acc = lambda entity: ( - G*M/entity.rr**2 ) + (entity.rr - 3/2 * R_S) * (entity.l0**2)/(entity.rr**4)
-
-        # game loop
-        while True:
-
-                                   
-            if(not self.sm.get('playerTurn')): # when computations are made ...
-                for k in range(nb_steps):
-                    screen.blit(background, (0,0))
-                    screen.blit(text_computing, (0,0))
-
-                    for entity in entity_pool.pool:
-                        # update quantities
-                        current = entity_pool.pool[entity]
-                        current.update(screen, radial_acc, dt)
-                        current.draw(screen)
-
-                        # test for collisions
-                        for other_entity in entity_pool.pool:
-                            other_current = entity_pool.pool[other_entity]
-                            if(current.id != other_entity and current.is_colliding_with(other_current)): # take advantage of lazyness
-                                pass
-
-                    pygame.display.flip()
-                    
-                #self.sm.set('playerTurn', True)
-
-
-            else: # in player's turn, just redraw objects at the same place
-                screen.blit(background, (0,0))
-                screen.blit(text_your_turn, (0,0))
-                bh.draw(screen)
-                for entity in entity_pool.pool:
-                    entity_pool.pool[entity].draw(screen)
-                pygame.display.flip()
-
-
-
-            # check exiting conditions
-            if(not player.palive or not target.palive):
-                self.has_won(screen, player.palive)                
-            self.should_game_exit()
-
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+   
 
         
     def run(self):
