33b4fd8594bad73e71f9851de4a452606301c465
atomicwelding <ctfweld@gmail.com>
2024-10-04 15:41:13 +0200

    refactor EntityPool

--- src/EntityPool.py
+++ src/EntityPool.py
@@ -1,18 +1,22 @@
 from IEntity import IEntity
 
-class EntityPool():
-
+class EntityPool:
+    """
+    Manages a pool of entities, allowing adding, removing, and drawing entities on the screen.
+    """
 
     def __init__(self):
         self.pool = {}
 
-
-    def add(self, entity : IEntity):
+    def add(self, entity: IEntity):
+        """Adds an entity to the pool."""
         self.pool[entity.id] = entity
 
-    def remove(self, entity : IEntity):
+    def remove(self, entity: IEntity):
+        """Removes an entity from the pool."""
         self.pool.pop(entity.id)
 
-    def  drawPool(self, screen):
-        for id in self.pool:
-            self.pool[id].draw()
+    def drawPool(self, screen):
+        """Draws all entities in the pool on the screen."""
+        for entity in self.pool.values():
+            entity.draw(screen)
