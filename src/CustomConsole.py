f6544a19e7d90c1d4209a23f8e41c1af2d11d978
atomicwelding <ctfweld@gmail.com>
2024-10-04 16:01:20 +0200

    refactor commands

--- src/CustomConsole.py
+++ src/CustomConsole.py
@@ -1,49 +1,76 @@
 from StateManager import StateManager
 from command_implementation import commands
-
 import code
 import sys
 
-
- 
-
 def command_handler(command, state_manager: StateManager):
+    """
+    Handles the execution of a command based on the input string.
+    
+    Parameters:
+    -----------
+    command : str
+        The input command from the user.
+    state_manager : StateManager
+        The game state manager used to execute commands.
+    
+    Returns:
+    --------
+    bool
+        Always returns True to indicate command handling is completed.
+    """
+    all_commands = [key for key in commands]
 
-        all_commands = [key for key in commands]
-
-        try:
-                parts = command.split()
-                command_name = parts[0]
-        except IndexError:
-                return True
-
-        if(not (command_name in all_commands)):
-                print("Please, use a defined command.")
-                print("Type `list` to list all the commands.")
-                return True
-
-        try:
-                arg1 = parts[1]
-        except IndexError:
-                arg1 = None
-
-        try:
-                arg2 = parts[2]
-        except IndexError:
-                arg2 = None
-
-        commands[command_name]['command'](state_manager, arg1, arg2)
-
+    try:
+        parts = command.split()
+        command_name = parts[0]
+    except IndexError:
         return True
 
-                
+    if command_name not in all_commands:
+        print("Please, use a defined command.")
+        print("Type `list` to list all the commands.")
+        return True
+
+    arg1 = parts[1] if len(parts) > 1 else None
+    arg2 = parts[2] if len(parts) > 2 else None
+
+    commands[command_name]['command'](state_manager, arg1, arg2)
+
+    return True
+
 class CustomConsole(code.InteractiveConsole):
+    """
+    A custom console for handling and executing game commands.
+    
+    Parameters:
+    -----------
+    state_manager : StateManager
+        The game state manager that tracks and executes commands.
+    """
+
     def __init__(self, state_manager):
         super().__init__()
         self.state_manager = state_manager
     
     def runsource(self, source, filename="<input>", symbol="single"):
-        if command_handler(source.strip(), state_manager = self.state_manager):
-            return False  # Do not execute the command further
+        """
+        Processes the input source and runs commands if they are valid.
+        
+        Parameters:
+        -----------
+        source : str
+            The command input from the user.
+        filename : str, optional
+            The filename to associate with the command (default is '<input>').
+        symbol : str, optional
+            The mode of input (default is 'single').
+        
+        Returns:
+        --------
+        bool
+            Returns False if the command is handled, otherwise proceeds with the standard interpreter.
+        """
+        if command_handler(source.strip(), state_manager=self.state_manager):
+            return False  # Command was handled, no further execution needed
         return super().runsource(source, filename, symbol)
-    
--- src/command_implementation.py
+++ src/command_implementation.py
@@ -1,42 +1,29 @@
 from StateManager import StateManager
-from params import * 
-
+from params import *
 import numpy as np
 import sys
 
-
 def exit_command(state_manager: StateManager, arg1, arg2):
-        state_manager.set('shouldExit', True)
-        sys.exit()
-
+    """Exits the game by setting the appropriate state and calling sys.exit()."""
+    state_manager.set('shouldExit', True)
+    sys.exit()
 
 def help_command(state_manager: StateManager, arg1, arg2):
-    if(not arg1):
+    """Displays help information for commands."""
+    if not arg1:
         print("Manual not written yet")
         return
 
-
     try:
-        print(commands[arg1]['use']  + " : " + commands[arg1]['goal'])
-
+        print(commands[arg1]['use'] + " : " + commands[arg1]['goal'])
     except KeyError:
         print("Command doesn't exist")
-    
-    if(not (arg1 in commands)):
-        print("Command doesn't exist")
-        return
-
-
-
-
-    
 
 def list_command(state_manager: StateManager, arg1, arg2):
-
-    if(not (arg1 == "variables")):
+    """Lists available commands or variables."""
+    if arg1 != "variables":
         for command in commands:
             print(command + " : " + commands[command]['goal'])
-
     else:
         variables = """distance : distance from black hole.
 radial_velocity : initial radial velocity
@@ -44,149 +31,126 @@ angular_velocity : initial angular velocity
 """
         print(variables)
 
-
 def run_command(state_manager: StateManager, arg1, arg2):
-        distance = state_manager.get('distance')
-        angular_velocity = state_manager.get('angular_velocity')
-        radial_velocity = state_manager.get('radial_velocity')
+    """Starts the game if required variables are set."""
+    distance = state_manager.get('distance')
+    angular_velocity = state_manager.get('angular_velocity')
+    radial_velocity = state_manager.get('radial_velocity')
 
-        if(distance != None and angular_velocity != None and radial_velocity != None):
-                state_manager.set('canRun', True)
-
-        if(not state_manager.get('canRun')):
-                print('Please, set distance, angular_velocity and radial_velocity before running the game.')
-
+    if all([distance, angular_velocity, radial_velocity]):
+        state_manager.set('canRun', True)
+    else:
+        print('Please, set distance, angular_velocity, and radial_velocity before running the game.')
 
 def set_command(state_manager: StateManager, arg1, arg2):
-    if(not (arg1 and arg2)):
+    """Sets game variables such as distance, radial_velocity, or angular_velocity."""
+    if not (arg1 and arg2):
         print('Please, set variables the proper way')
         print(commands['set']['use'])
         return
-    else:
-        try:
-            if(arg1 == 'distance'):
-                state_manager.set('distance', float(arg2) * R_S)
-                            
-            elif(arg1 == 'radial_velocity'):
-                state_manager.set('radial_velocity', float(arg2))
-                            
-            elif(arg1 == 'angular_velocity'):
-                state_manager.set('angular_velocity', float(arg2))
-            else:
-                print('Variable name not known. Type `list variables` to list all the variables.')
-        except ValueError:
-            print('Value not valid. Please, use a number.')
-
+    try:
+        if arg1 == 'distance':
+            state_manager.set('distance', float(arg2) * R_S)
+        elif arg1 == 'radial_velocity':
+            state_manager.set('radial_velocity', float(arg2))
+        elif arg1 == 'angular_velocity':
+            state_manager.set('angular_velocity', float(arg2))
+        else:
+            print('Variable name not known. Type `list variables` to list all the variables.')
+    except ValueError:
+        print('Value not valid. Please, use a number.')
 
 def sdbg_command(state_manager: StateManager, arg1, arg2):
+    """Debugging command to set predefined values for game variables."""
     state_manager.set('distance', 2 * R_S)
     state_manager.set('angular_velocity', 30.0)
     state_manager.set('radial_velocity', 30.)
 
-
 def shoot_command(state_manager: StateManager, arg1, arg2):
-        if(not state_manager.get('canRun')):
-                print('Game is not running.')
-                return
-        if(not state_manager.get('playerTurn')):
-                print('Wait for your turn')
-                return
-        
-        if(arg1 not in ['heavy', 'light']):
-                print("Not a valid projectile type")
-                return
+    """Handles shooting action, checking ammo availability and setting shot parameters."""
+    if not state_manager.get('canRun'):
+        print('Game is not running.')
+        return
+    if not state_manager.get('playerTurn'):
+        print('Wait for your turn')
+        return
+    
+    if arg1 not in ['heavy', 'light']:
+        print("Not a valid projectile type")
+        return
+    if arg1 == 'heavy' and not state_manager.get('playerCanShootHeavy'):
+        print("No heavy ammo left")
+        return
+    if arg1 == 'light' and not state_manager.get('playerCanShootLight'):
+        print("No light ammo left")
+        return
 
-        if(arg1 == 'heavy' and not state_manager.get('playerCanShootHeavy')):
-                print("No heavy ammo left")
-                return
-        if(arg1 == 'light' and not state_manager.get('playerCanShootLight')):
-                print("No light ammo left")
-                return
+    state_manager.set('playerShotType', arg1)
+    try:
+        angle = float(arg2) * np.pi
+        state_manager.set('playerShotAngle', angle)
+    except (ValueError, TypeError):
+        print('Please, provide a valid multiple of pi')
+        return
 
-        state_manager.set('playerShotType', arg1)
-        try:
-                angle = float(arg2) * np.pi
-                state_manager.set('playerShotAngle', angle)
-        except ValueError:
-                print('Please, provide a valid multiple of pi')
-                return
-        except TypeError:
-                print('Please, provide an angle')
-                return
-
-        state_manager.set('playerShot', True)
-
-        # next step 
-        step_command(state_manager, arg1, arg1)
-
+    state_manager.set('playerShot', True)
+    step_command(state_manager, arg1, arg2)
 
 def step_command(state_manager: StateManager, arg1, arg2):
-        state_manager.set('playerTurn', False)
-
+    """Proceeds to the next step by ending the player's turn."""
+    state_manager.set('playerTurn', False)
 
 def wait_command(state_manager: StateManager, arg1, arg2):
-        if(not state_manager.get('canRun')):
-                print('Game is not running.')
-                return
-        if(not state_manager.get('playerTurn')):
-                print('Wait for your turn')
-                return
+    """Waits for the next turn, allowing the player to skip their current turn."""
+    if not state_manager.get('canRun'):
+        print('Game is not running.')
+        return
+    if not state_manager.get('playerTurn'):
+        print('Wait for your turn')
+        return
+    step_command(state_manager, arg1, arg2)
 
-        step_command(state_manager, arg1, arg2)
-        
+# Global command definitions
 global commands
 commands = {
-        
-        'exit': {
-                'command': exit_command,
-                'use': '`exit`',
-                'goal': 'exit the game',
-        },
-        
-        'help': {
-                'command': help_command,
-                'use': '`help [(command_name)]`',
-                'goal': 'print a short manual if no command_name is providen ; helps with the command if provided',
-        },
-        
-        'list': {
-                'command': list_command,
-                'use': '`list [variables|(commands)]`',
-                'goal': 'list all variables or commands, depending on the argument you pass ; list all the commands by default',
-        },
-        
-        'run': {
-                'command': run_command,
-                'use': '`run`',
-                'goal': 'run the game ; you need to  `set` variables before it runs'
-        },
-
-        'set': {
-                'command': set_command,
-                'use': '`set [distance (as multiple of variable)|angular_velocity|radial_velocity]`',
-                'goal': 'set a value to a given variable'
-        },
-
-        'sdbg': {
-                'command': sdbg_command,
-                'use': '`sdbg`',
-                'goal': 'debugging purpose only'
-        },
-        'shoot': {
-                'command':shoot_command,
-                'use': '`shot [heavy|light] angle_multiple_of_pi`',
-                'goal': 'shot a projectile'
-        },
-        'wait' : {
-                'command':wait_command,
-                'use':'`wait`',
-                'goal': 'wait for a more favorable position'
-        }
+    'exit': {
+        'command': exit_command,
+        'use': '`exit`',
+        'goal': 'exit the game',
+    },
+    'help': {
+        'command': help_command,
+        'use': '`help [(command_name)]`',
+        'goal': 'print a short manual if no command_name is providen ; helps with the command if provided',
+    },
+    'list': {
+        'command': list_command,
+        'use': '`list [variables|(commands)]`',
+        'goal': 'list all variables or commands, depending on the argument you pass ; list all the commands by default',
+    },
+    'run': {
+        'command': run_command,
+        'use': '`run`',
+        'goal': 'run the game ; you need to `set` variables before it runs'
+    },
+    'set': {
+        'command': set_command,
+        'use': '`set [distance (as multiple of variable)|angular_velocity|radial_velocity]`',
+        'goal': 'set a value to a given variable'
+    },
+    'sdbg': {
+        'command': sdbg_command,
+        'use': '`sdbg`',
+        'goal': 'debugging purpose only'
+    },
+    'shoot': {
+        'command': shoot_command,
+        'use': '`shoot [heavy|light] angle_multiple_of_pi`',
+        'goal': 'shoot a projectile'
+    },
+    'wait': {
+        'command': wait_command,
+        'use': '`wait`',
+        'goal': 'wait for a more favorable position'
+    }
 }
-
-
-
-
-
-
-        
