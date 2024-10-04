6995fc8cba4c8e03dceba14c6e2977d8de32c40b
atomicwelding <ctfweld@gmail.com>
2024-10-04 15:38:56 +0200

    refactor IEntity

--- src/IEntity.py
+++ src/IEntity.py
@@ -1,25 +1,40 @@
 from params import * 
-
 from abc import ABC, abstractmethod
 from uuid import uuid4
 import numpy as np
 import os
-
-
 import pygame
 
-
-
-
-
 class IEntity(ABC):
-
-    def __init__(self,
-                 img_path,
-                 mass,
-                 rr, rtheta,
-                 rdot, thetadot):
-
+    """
+    Abstract base class representing an entity in the game. 
+    Each entity is defined by its position, velocity, and graphical representation.
+    
+    Attributes:
+    -----------
+    id : str
+        Unique identifier for the entity.
+    mass : float
+        Mass of the entity.
+    rr : float
+        Radial distance from the origin.
+    rtheta : float
+        Angular position in polar coordinates.
+    rdot : float
+        Radial velocity.
+    thetadot : float
+        Angular velocity.
+    palive : bool
+        Whether the entity is alive.
+    l0 : float
+        Angular momentum constant for the entity's motion.
+    img_path : str
+        Path to the entity's image.
+    surface : pygame.Surface
+        Loaded image surface for the entity.
+    """
+    
+    def __init__(self, img_path, mass, rr, rtheta, rdot, thetadot):
         self.id = uuid4().hex
         self.mass = mass
         self.rr = rr
@@ -27,13 +42,9 @@ class IEntity(ABC):
         self.rdot = rdot
         self.thetadot = thetadot
         self.palive = True
-
         self.l0 = self.rr ** 2 * self.thetadot
-        
         self.img_path = img_path
-
         self.surface = self.load()
-
 
     def init(self):
         self.l0 = self.rr ** 2 * self.thetadot
@@ -51,12 +62,10 @@ class IEntity(ABC):
         return np.sqrt(self.radial_velocity()** 2 + self.angular_velocity()**2)
 
     def radius(self):
-        # Define radius as a fraction of the surface's width, or use a custom value
         sfw, sfh = self.surface.get_size()
-        return (sfw + sfh) / 4  # Example: half the average dimension of the surface
+        return (sfw + sfh) / 4
 
-
-    def load(self) -> pygame.Surface :
+    def load(self) -> pygame.Surface:
         return pygame.image.load(os.path.join(ASSETS_PATH, self.img_path)).convert_alpha()
 
     def x(self):
@@ -64,33 +73,28 @@ class IEntity(ABC):
 
     def y(self):
         return self.rr * np.sin(self.rtheta)
-    
+
     def pyg_coords(self, scene):
-        # translating in pygame's coordinate
         sfw, sfh = self.surface.get_size()
         scw, sch = scene.get_size()
+        x = (self.x() - sfw // 2) + scw // 2
+        y = (self.y() - sfh // 2) + sch // 2
+        return (x, y)
 
-        x = (self.x() - sfw//2) + scw//2
-        y = (self.y() - sfh//2) + sch//2
-
-        return (x,y)
-
-    def update(self, scene, radial_acceleration, dt : float) -> None :
-        self.thetadot = self.l0 / self.rr*2
-        self.rtheta += self.l0*dt / self.rr**2
+    def update(self, scene, radial_acceleration, dt: float) -> None:
+        self.thetadot = self.l0 / self.rr**2
+        self.rtheta += self.l0 * dt / self.rr**2
         self.rdot += dt / 2 * radial_acceleration(self)
         self.rr += self.rdot * dt
         self.rdot += dt / 2 * radial_acceleration(self)
-        
-        scw, sch = scene.get_size()
 
-        if(self.x() < -scw/2 or self.x() > scw/2
-           or self.y() < - sch/2 or self.y() > sch/2):
-            self.rr = scw/2
+        scw, sch = scene.get_size()
+        if (self.x() < -scw/2 or self.x() > scw/2
+            or self.y() < - sch/2 or self.y() > sch/2):
+            self.rr = scw / 2
             self.rtheta += np.pi
             self.rdot *= -1
-            
-            
+
     def draw(self, scene):
         scene.blit(self.surface, self.pyg_coords(scene))
 
@@ -100,41 +104,21 @@ class IEntity(ABC):
         return rectA.colliderect(rectB)
 
     def assign_result_collisions(self, other):
-        m1 = self.mass
-        m2 = other.mass
+        m1, m2 = self.mass, other.mass
+        T1, T2 = self.angular_velocity(), other.angular_velocity()
+        v1, v2 = self.radial_velocity(), other.radial_velocity()
 
-        T1 = self.angular_velocity()
-        T2 = other.angular_velocity()
-
-        v1 = self.radial_velocity()
-        v2 = other.radial_velocity()
-
-        # Compute post-collision angles and velocities
         T1prime = np.atan(((m1 - m2) / (m1 + m2)) * np.tan(T1) + (2 * m2 / (m1 + m2)) * (v2 / v1) * np.sin(T2) / np.cos(T1))
         v1prime = np.sqrt((((m1 - m2) / (m1 + m2)) * v1 * np.sin(T1) + (2 * m2 / (m1 + m2)) * v2 * np.sin(T2)) ** 2 + (v1 * np.cos(T1)) ** 2)
-        
         T2prime = np.atan(((m2 - m1) / (m1 + m2)) * np.tan(T2) + (2 * m1 / (m1 + m2)) * (v1 / v2) * np.sin(T1) / np.cos(T2))
         v2prime = np.sqrt((((m2 - m1) / (m1 + m2)) * v2 * np.sin(T2) + (2 * m1 / (m1 + m2)) * v1 * np.sin(T1)) ** 2 + (v2 * np.cos(T2)) ** 2)
 
-        self.rdot = v1prime
-        self.thetadot = T1prime / self.rr
+        self.rdot, self.thetadot = v1prime, T1prime / self.rr
+        other.rdot, other.thetadot = v2prime, T2prime / other.rr
 
-        other.rdot = v2prime
-        other.thetadot = T2prime / other.rr
-        
-        # --- Position Correction to Avoid Overlap ---
-        # Calculate the distance between the two ships using polar coordinates
-        distance = np.sqrt(self.rr**2 + other.rr**2 - 2*self.rr*other.rr*np.cos(self.rtheta - other.rtheta))
-
-        # Sum of radii to check for overlap
+        distance = np.sqrt(self.rr**2 + other.rr**2 - 2 * self.rr * other.rr * np.cos(self.rtheta - other.rtheta))
         overlap_distance = self.radius() + other.radius() - distance
 
-        if overlap_distance > 0:  # If there's an overlap
-            # Unit vector direction in polar coordinates
-            direction_rtheta = (self.rtheta - other.rtheta) / distance
-        
-            # Push each ship apart by half the overlap distance
+        if overlap_distance > 0:
             self.rr += overlap_distance / 2
             other.rr -= overlap_distance / 2
-
-        
