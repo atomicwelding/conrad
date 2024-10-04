fd03825c3861fded3e4c05846ec4fe071beab659
atomicwelding <ctfweld@gmail.com>
2024-10-04 15:54:24 +0200

    refactor projectiles

--- src/HeavyProjectile.py
+++ src/HeavyProjectile.py
@@ -1,27 +1,44 @@
 from Projectile import Projectile
 from LightProjectile import LightProjectile
-
 import numpy as np 
 
-
 class HeavyProjectile(Projectile):
+    """
+    Represents a heavy projectile with a specific speed and direction.
+    
+    Parameters:
+    -----------
+    mass : float
+        The mass of the projectile.
+    rr : float
+        Radial distance from the origin.
+    rtheta : float
+        Angular position in polar coordinates.
+    sign : int, optional
+        Direction of the projectile's velocity (1 for positive, -1 for negative), default is 1.
+    """
 
-    def __init__(self, mass, rr, rtheta, sign = 1):
-        super().__init__(img_path = 'heavy-projectile.png',
-                         mass = mass,
-                         rr = rr,
-                         rtheta = rtheta,
-                         rdot = 0,
-                         thetadot = sign * 12 / rr)
+    def __init__(self, mass, rr, rtheta, sign=1):
+        super().__init__(img_path='heavy-projectile.png',
+                         mass=mass,
+                         rr=rr,
+                         rtheta=rtheta,
+                         rdot=0,
+                         thetadot=sign * 12 / rr)
 
-
-        
     def is_colliding_with(self, entity):
+        """
+        Handles collisions with other projectiles, either heavy or light.
+        
+        Parameters:
+        -----------
+        entity : IEntity
+            The entity to check collision with.
+        """
         pcolliding = super().is_colliding_with(entity)
 
-        if(pcolliding and isinstance(entity, HeavyProjectile)):
-           self.assign_result_collisions(entity)
-
-        if(pcolliding and isinstance(entity, LightProjectile)):
+        if pcolliding and isinstance(entity, HeavyProjectile):
             self.assign_result_collisions(entity)
- 
+
+        if pcolliding and isinstance(entity, LightProjectile):
+            self.assign_result_collisions(entity)
--- src/LightProjectile.py
+++ src/LightProjectile.py
@@ -1,13 +1,25 @@
 from Projectile import Projectile
 
-
-
 class LightProjectile(Projectile):
+    """
+    Represents a light projectile with a higher speed compared to heavy projectiles.
+    
+    Parameters:
+    -----------
+    mass : float
+        The mass of the projectile.
+    rr : float
+        Radial distance from the origin.
+    rtheta : float
+        Angular position in polar coordinates.
+    sign : int, optional
+        Direction of the projectile's velocity (1 for positive, -1 for negative), default is 1.
+    """
 
-    def __init__(self, mass, rr, rtheta, sign = 1):
-        super().__init__(img_path = 'light-projectile.png',
-                         mass = mass,
-                         rr = rr,
-                         rtheta = rtheta,
-                         rdot = 0,
-                         thetadot = sign * 40 / rr)
+    def __init__(self, mass, rr, rtheta, sign=1):
+        super().__init__(img_path='light-projectile.png',
+                         mass=mass,
+                         rr=rr,
+                         rtheta=rtheta,
+                         rdot=0,
+                         thetadot=sign * 40 / rr)
